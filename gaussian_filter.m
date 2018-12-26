a6828365aa957983e5f8ee375ec8955ffaaeecb2
Aditya Goel <adityagoel@Adityas-MacBook-Air.local>
2018-12-27 02:26:14 +0530

    Lecture 2A-L3 completed

--- gaussian_filter.m
+++ gaussian_filter.m
@@ -11,9 +11,12 @@ imshow(noisy_image);
 %Create a Gaussian Filter
 filter_size = 11;
 filter_sigma = 2;
-%If you're using Octave, use "pkg load img" before next statement
+%If you're using Octave, use "pkg load image" before next statement
 filter = fspecial('gaussian', filter_size, filter_sigma);
 
 %Applying Filter to remove noise
-smoothed = imfilter(noisy_image, filter);
+smoothed = imfilter(noisy_image, filter,'symmetric');
+% Avaialbe options in imfilter are
+% clip edge (black seeps in, use '0'), wrap around (circular), 
+% copy edge -> replicate, reflect edge(symmetric)
 imshow(smoothed);
