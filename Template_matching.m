d4c0b91078bb1e3272267946362663c9cfc26cee
Aditya Goel <aditya2.7182@gmail.com>
2019-01-03 23:40:15 +0530

    Added comments to explain raw index

--- Template_matching.m
+++ Template_matching.m
@@ -9,7 +9,12 @@ find_template_1D(template, signal);
 function index = find_template_1D(t, s)
     %We use normalised cross correlation here
     nxc = normxcorr2(t, s);
+    %The normxcorss function doesn't return the index where matching begins
+    %Rather it returns the index where matching ends
     [~, rawIndex] = max(nxc);
+    %Hence, we need to extract a raw index from nxc
+    %And then subtract the size of filter(template) and add 1 to get the
+    %final output index
     index = rawIndex - size(t, 2) + 1;
     disp("The Index where the template(filter) matches the signal most is at:");
     disp(index);
